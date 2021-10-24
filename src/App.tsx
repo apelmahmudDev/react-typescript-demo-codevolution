8064f9fc1b3e6f0ae29b5779bd85800c4053ff76
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-24 21:16:58 +0600

    done Array of object data type with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Greet from "./components/Greet";
 import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 const App = () => {
 	const personName = {
@@ -7,10 +8,26 @@ const App = () => {
 		last: "Mahmud",
 	};
 
+	const nameList = [
+		{
+			first: "Apel",
+			last: "Mahmud",
+		},
+		{
+			first: "Sharmin",
+			last: "Jannat",
+		},
+		{
+			first: "Solayman",
+			last: "Shukhon",
+		},
+	];
+
 	return (
 		<div>
 			<Greet name="Apel Mahmud" messageCount={10} isLoggedIn={false} />
 			<Person name={personName} />
+			<PersonList names={nameList} />
 		</div>
 	);
 };
