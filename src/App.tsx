87c745c4c4714b39d4362e32a673476cda03253a
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-24 21:01:48 +0600

    Person comp done with object type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import Greet from "./components/Greet";
+import Person from "./components/Person";
 
 const App = () => {
-	return <Greet name="Apel Mahmud" messageCount={10} isLoggedIn={false} />;
+	const personName = {
+		first: "Apel",
+		last: "Mahmud",
+	};
+
+	return (
+		<div>
+			<Greet name="Apel Mahmud" messageCount={10} isLoggedIn={false} />
+			<Person name={personName} />
+		</div>
+	);
 };
 export default App;
