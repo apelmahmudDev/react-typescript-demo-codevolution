a02649ed0c8fb73a1cf34c538e93e24629cd3a93
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-25 06:03:13 +0600

    input change handler with typescript added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import Status from "./components/Status";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Greet from "./components/Greet";
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 const App = () => {
 	return (
 		<div>
-			<Status status="loading" />
-			<Oscar>
-				<Heading>Lorem ipsum dolor sit amet consectetur.</Heading>
-			</Oscar>
-			<Greet name="Apel Mahmud" isLoggedIn={true} />
 			<Button handleClick={(event, id) => console.log("clicked", event, id)} />
+			<Input
+				value="Hello bangladesh"
+				handleChange={(event) => console.log("clicked")}
+			/>
 		</div>
 	);
 };
