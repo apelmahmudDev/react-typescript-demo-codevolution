ea004cd0bba84063e7bfcd6551846d52da65fa88
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-10-25 07:26:27 +0600

    Loggedin comp ready to check type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
+import PersonList from "./components/PersonList";
 
 const App = () => {
+	const nameList = [
+		{ first: "Apel", last: "Mahmud" },
+		{ first: "Karim", last: "Mahmud" },
+	];
+
 	return (
 		<div>
+			<PersonList names={nameList} />
 			<Button handleClick={(event, id) => console.log("clicked", event, id)} />
 			<Input
 				value="Hello bangladesh"
